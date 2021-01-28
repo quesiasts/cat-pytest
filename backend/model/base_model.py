9395953b6c26774f5be9fb22b5a28303cf3fc3d8
Quesia <quesiasts@gmail.com>
2021-01-28 15:41:49 -0300

    main vazio

--- backend/model/base_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer
-
-Base = declarative_base()
-
-class BaseModel:
-    __abstract__ = True
-    id = Column(Integer, primary_key = True)
-
